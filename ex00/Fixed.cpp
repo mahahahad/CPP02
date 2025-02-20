cfd0d3694722885283e1cfec4f994355f07c8bc9
mahahahad <mahad.abd2003@gmail.com>
2025-02-20 22:45:08 +0400

    style: migrate from tabs to spaces
    also add comments

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,31 +1,41 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed() {
-	value = 0;
-	cout << "Default constructor called" << endl;
+    value = 0;
+    cout << "Default constructor called" << endl;
 }
 
 Fixed::Fixed(const Fixed &copy) {
-	cout << "Copy constructor called" << endl;
+    cout << "Copy constructor called" << endl;
     value = copy.getRawBits();
 }
 
 Fixed::~Fixed() {
-	cout << "Destructor called" << endl;
+    cout << "Destructor called" << endl;
 }
 
 Fixed &Fixed::operator=(const Fixed& t) {
-	cout << "Copy assignment operator called" << endl;
-	value = t.getRawBits();
-	return (*this);
+    cout << "Copy assignment operator called" << endl;
+    value = t.getRawBits();
+    return (*this);
 }
 
-int		Fixed::getRawBits(void) const {
-	cout << "getRawBits member function called" << endl;
-	return (value);
+/**
+ * @brief Get the raw value of the Fixed point number
+ * 
+ * @return int The integer representation of the Fixed point number
+ */
+int     Fixed::getRawBits(void) const {
+    cout << "getRawBits member function called" << endl;
+    return (value);
 }
 
-void	Fixed::setRawBits(int const raw) {
+/**
+ * @brief Manually change the raw value of the Fixed point number
+ * 
+ * @param raw The new value to overwrite with
+ */
+void    Fixed::setRawBits(int const raw) {
     value = raw;
-	cout << "setRawBits member function called" << endl;
+    cout << "setRawBits member function called" << endl;
 }
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -8,16 +8,16 @@ using std::cout;
 using std::endl;
 
 class Fixed {
-	public:
-		Fixed();
-		Fixed( const Fixed &copy );
-		Fixed	&operator=(const Fixed& x);
-		~Fixed();
-		int		getRawBits( void ) const;
-		void	setRawBits( int const raw );
-	private:
-		int	value;
-		static const int fractionalBits = 8;
+    public:
+        Fixed();
+        Fixed( const Fixed &copy );
+        Fixed	&operator=( const Fixed& x );
+        ~Fixed();
+        int		getRawBits( void ) const;
+        void	setRawBits( int const raw );
+    private:
+        int	value;
+        static const int fractionalBits = 8;
 };
 
 #endif
